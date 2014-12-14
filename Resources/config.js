e470828c69400bc9e54d7000ebfb038d86fa22ea
motoy3d <motoy3d@gmail.com>
2014-12-15 04:07:33 +0900

    first commit

---
+++ Resources/config.js
@@ -0,0 +1,27 @@
+exports.config = {
+    teamId: 'fctokyo'
+    ,hashtag: 'fctokyo'
+    ,teamName: 'FC東京'
+    ,appName: 'スマートJ for FC東京'
+    ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id920319272?l=ja&ls=1&mt=8'
+    ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.fctokyo'
+    ,color: '#0000ff'
+    ,tintColor: 'white'
+    ,dbName: 'smartj.fctokyo'
+    //ニュースのソースURL
+    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    //日程・結果のソースURL
+    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=fctokyo&season="
+    //設定、メッセージのソースURL
+    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=fctokyo"
+    ,newsEntriesPerPage: 50
+    ,googleAnalyticsTrackingId: 'UA-30928840-1'
+    //nend(iPhone)
+    ,nendSpotIdIPhoneIcon: 236044
+    ,nendApiKeyIPhoneIcon: 'f29cda28d48c667d3b1237e312b6cec9c66f1dd8'
+    ,nendSpotIdIPhoneBanner: 236030
+    ,nendApiKeyIPhoneBanner: '9692a7dbc9de47c437315313e9327291cbe69a2f'
+    //nend(Android)
+    ,nendSpotIdAndroid: 235290
+    ,nendApiKeyAndroid: 'ca63a0f89970e2ea6bc43ba875a6bb28164f3524'
+};
