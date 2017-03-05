328191ce368fd019604603a3c4418bb03e8d9636
motoy3d <motoy3d@gmail.com>
2017-03-05 15:23:24 +0900

    動作が遅い部分を改善しました！ 他チームのニュースが見れるようになりました！ 2017シーズンに対応しました！

--- Resources/config.js
+++ Resources/config.js
@@ -2,18 +2,40 @@ exports.config = {
     teamId: 'fctokyo'
     ,hashtag: 'fctokyo'
     ,teamName: 'FC東京'
+    ,teamNameFull: 'FC東京'
     ,appName: 'スマートJ for FC東京'
     ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id920319272?l=ja&ls=1&mt=8'
     ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.fctokyo'
+    ,backgroundColor: 'black'
+	,visitedBgColor : '#ddd'
+    ,mainTextColor: 'white'
     ,color: '#0000ff'
     ,tintColor: 'white'
+    ,standingsBgcolor: '#0000ff'
+    ,standingsTeamFontColor: 'white'
+    ,resultsDetailBtnBgColorActive: '#333'
+    ,resultsDetailBtnColorActive: 'white'
+    ,resultsDetailBtnBgColorInactive: '#202020'
+    ,resultsDetailBtnColorInactive: '#444'
     ,dbName: 'smartj.fctokyo'
+    //URLベース
+    ,urlBase: 'https://smartj.mobi/redsmylife/'
     //ニュースのソースURL
-    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    ,feedUrlBase: 'https://smartj.mobi/redsmylife/news.json'
     //日程・結果のソースURL
-    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=fctokyo&season="
+    ,resultsUrl: "https://smartj.mobi/redsmylife/results.json?teamId=fctokyo&season="
+    //順位表のソースURL
+    ,standingsUrl: 'https://smartj.mobi/redsmylife/standings.json'
     //設定、メッセージのソースURL
-    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=fctokyo"
+    ,messageUrl: "https://smartj.mobi/redsmylife/config.json?teamId=fctokyo"
+    //動画のソースURL
+    ,videoUrl: "https://smartj.mobi/redsmylife/video.json?teamId=fctokyo"
+    //報告URL
+    ,reportUrl: "https://smartj.mobi/redsmylife/report.json?teamId=antlers"
+    //利用規約URL
+    ,rulesUrl: "https://smartj.mobi/redsmylife/rules.jsp?appName="
+    //開発元メールアドレス
+    ,developerMail: "motoy3d+smartj@gmail.com"
     ,newsEntriesPerPage: 50
     ,googleAnalyticsTrackingId: 'UA-30928840-1'
     //nend(iPhone)
@@ -24,4 +46,6 @@ exports.config = {
     //nend(Android)
     ,nendSpotIdAndroid: 235290
     ,nendApiKeyAndroid: 'ca63a0f89970e2ea6bc43ba875a6bb28164f3524'
+    ,nendSpotIdAndroidBanner: 393104
+    ,nendApiKeyAndroidBanner: 'a2e0aae30ff1a245881c075558f62bc868000a93'
 };
